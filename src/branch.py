c7db4c7a93e916be30605ad74096b36bf0329f6d
nir-valtman <nir@arnica.io>
2021-12-25 15:28:16 -0500

    Added PR reviews and branch protection

--- src/branch.py
+++ src/branch.py
@@ -4,6 +4,7 @@ class Branch:
 
     def __init__(self, organization, config_file = None):
         self.endpoint = f'/repos/{organization}/[REPO]/git/refs'
+        self.branch_protection_endpoint = f'/repos/{organization}/[REPO]/branches/[BRANCH]/protection'
         self.config_file = config_file
 
     async def get_main(self, pat, repository):
@@ -20,5 +21,32 @@ class Branch:
             'sha': source_branch_sha
         }
         resp = await conn.post(endpoint, json_data=data)
+        return resp
+    
+    async def set_branch_protection(self, repository, branch_name, restricted_users = [], restricted_teams = []):
+        conn = ConnectionHandler(config_file=self.config_file)
+        endpoint = self.branch_protection_endpoint.replace('[REPO]',repository).replace('[BRANCH]', branch_name)
+        payload = {
+            'required_status_checks': {
+                'strict': False,
+                'contexts': []
+            },
+            'enforce_admins': False,
+            'require_code_owner_reviews': False,
+            'required_pull_request_reviews': {
+                'required_approving_review_count': 1
+            },
+            'restrictions': {
+                'users': restricted_users,
+                'teams': restricted_teams
+            },
+            'allow_force_pushes': True,
+            'allow_deletions': True,
+            'bypass_pull_request_allowances': {
+                'users': restricted_users,
+                'teams': restricted_teams
+            }
+        }
+        resp = await conn.put(endpoint, payload)
         return resp
       
